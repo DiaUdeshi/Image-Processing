aabccdabfc17622c22f48fc114308fedcb12fee9
DiaUdeshi <127831605+DiaUdeshi@users.noreply.github.com>
2024-05-22 02:57:07 -0400

    Delete color_mask.py

--- color_mask.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-img=cv2.imread('wallpaper-night.jpg')
-hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-lower=np.array([110,50,50])
-upper=np.array([130,255,255])
-mask=cv2.inRange(hsv,lower,upper)
-result=cv2.bitwise_and(img,img,mask=mask)
-cv2.imwrite("blue_mask_output.jpg",result)
