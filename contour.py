f965c5b5c4e92b8c3176bdb4d94b78eae799a260
DiaUdeshi <127831605+DiaUdeshi@users.noreply.github.com>
2024-05-22 02:57:43 -0400

    Delete contour.py

--- contour.py
+++
@@ -1,6 +0,0 @@
-import cv2
-img=cv2.imread('wallpaper-night.jpg')
-edges=cv2.Canny(img,100,200)
-contours,_=cv2.findContours(edges,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-cv2.drawContours(img,contours,-1,(0,255,0),1)
-cv2.imwrite("contoured.jpg",img)
